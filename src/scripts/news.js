4f1812ca1b83600dcd5e496bb0d362acff5ca204
Meg Cruzen <megcruzen@gmail.com>
2019-01-10 10:34:39 -0600

    Added some planning notes

--- src/scripts/news.js
+++ src/scripts/news.js
@@ -1,6 +1,23 @@
 // Creates and displays News Articles section
 // Author: Megan Cruzen
 
+import newsData from "./data"
+
 const newsArticles = {
+
+    buildArticles() {
+
+        // GET data
+
+        // Clear output container on DOM
+
+        // Create news container
+        // Add Title
+        // Add "Add New" button - attach event listener and display form
+
+        // Loop through items and build each news item
+
+        // Append each item to news container
+    }
 
 }
