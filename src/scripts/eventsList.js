7c6c9bdc872435f4f63911d4d0a6064c7e00063a
Brittany Ramos-Janeway <bramosjaneway@gmail.com>
2019-01-10 12:28:00 -0600

    small changes

---
+++ src/scripts/eventsList.js
@@ -0,0 +1,13 @@
+import events from "./events"
+import data from "./data"
+
+const eventsList = {
+    listEvents(){
+        // get events from database
+        .then(event => {
+            event.forEach( )
+        })
+    }
+}
+
+export default eventsList
