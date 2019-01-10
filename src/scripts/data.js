3684d9568f3f60c36e63eac5ac01e807f7e04545
Meg Cruzen <megcruzen@gmail.com>
2019-01-10 11:49:20 -0600

    Added basic HTML structure for news articles list

---
+++ src/scripts/data.js
@@ -0,0 +1,13 @@
+// object that contains all fetch calls for the database JSON file
+const data = {
+    getData(data) {
+        return fetch(`http://localhost:8088/${data}`)
+        .then(response => response.json())
+    },
+    newsData() {
+        return fetch("http://localhost:8088/articles?userId=1") // pass through userID and change to ${userID}
+        .then(response => response.json())
+    }
+};
+
+export default data
