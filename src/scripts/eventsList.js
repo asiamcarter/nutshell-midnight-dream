910ee2d326fb95d2dd98e59353ee5ecb938ce24d
Brittany Ramos-Janeway <bramosjaneway@gmail.com>
2019-01-14 09:40:00 -0600

    events are sorted according to date

--- src/scripts/eventsList.js
+++ src/scripts/eventsList.js
@@ -14,17 +14,21 @@ const eventsList = {
         // get events from database
         data.getData(eventString)
             .then(allEvents => {
+                allEvents.sort(function(a, b){
+                    return new Date(a.date) - new Date(b.date);
+                });
+
                 // declare document fragment that will grab all events as they are built into HTML elements
                 let docFrag = document.createDocumentFragment();
 
                 // add HTML for each event in the database and add to document fragment
                 allEvents.forEach(event => {
-                    let eventIteration = events.eventBuilder;(event.id, event);
+                    let eventIteration = events.eventBuilder(event.id, event);
                     docFrag.appendChild(eventIteration);
                 })
 
                 // add document fragment to the container
-                let eventDiv = document.querySelector;(".posted--container");
+                let eventDiv = document.querySelector(".posted--container");
                 eventDiv.appendChild(docFrag);
         })
     }
